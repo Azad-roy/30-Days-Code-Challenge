4fa7f46723adbf23feceaded38065a6b9230640a
Azad Kumar <azadr1233@gmail.com>
2024-02-26 20:27:57 +0530

    Here empty string
    Maine yha pr reverse element save krne kele a empty string bnaya h,or use me save krwa kr,fir uska empty string ko fill kr de aa,or us fill ke aa hu aa string ko user ke dwara le aa gya element ko,if ke dwara equal dekha de aa,agar equal hu aa to to palindrome,nhi to palindrome nhi h😊

--- Day 1/20_palindrome.py
+++ Day 1/20_palindrome.py
@@ -1,55 +1,11 @@
-# # palindrome=input("Enter any word or sentence: ")
-# # for i in range(0,len(palindrome)):
-# #     if(palindrome[i]==palindrome[len(palindrome)-1]):
-# #         print("Yes")
-
-# def is_palindrome(s):
-#     # Remove spaces and convert to lowercase
-#     s = s.replace(" ", "").lower()
-    
-#     # Compare the original string with its reverse
-#     return s == s[::-1]
-
-# # Test the function
-# string_to_check = input("Enter a string: ")
-# if is_palindrome(string_to_check):
-#     print("Yes, it's a palindrome!")
-# else:
-#     print("No, it's not a palindrome.")
-
-palindrome =input("Enter any word or sentence: ")
-for i in range(len(palindrome),0,-1):
-    # a=print(palindrome[i-1],end="")
-    a=palindrome[i-1]
-    
-for i in range(len(palindrome),0,-1):
-    print(a[i-1],end="")
-# print(type(a))
-# if palindrome in a:
-#     print("this is a palindrome")
-# else:
-#     print("This is not palindrome")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def reverse(str):
+    a=""
+    for i in range(len(str),0,-1):
+        a=a+str[i-1]
+    return a
+str=input("Enter any word or sentence: ")
+if reverse(str) in str:
+    # print(reverse(str))
+    print(f"{str}; is palindrome")
+else:
+    print(f"{str}; is not palindrome")
