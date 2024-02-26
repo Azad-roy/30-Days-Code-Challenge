cbc64c89284d9650d2b820eda9d84de98b0038b0
Azad Kumar <azadr1233@gmail.com>
2024-02-26 20:29:33 +0530

    simple h
    aik baar dhaiyan se code ko dekho smjh ja o ge,nhi to gpt se puchh lo kaise work kr rha h code,nhi to Hentai💦 se puchh lo

--- Day 1/18_squared.py
+++ Day 1/18_squared.py
@@ -1,9 +1,10 @@
 # Create a list of numbers
 numbers = [1, 2, 3, 4, 5]
-
+list2=[]
+list3=[]
 # Use list comprehension to create a new list with squared elements
-squared_numbers = [x ** 2 for x in numbers]
-
-# Print the original list and the new list with squared elements
-print("Original list of numbers:", numbers)
-print("List with squared elements:", squared_numbers)
+for i in numbers:
+     list2.append(i**2)
+     list3.append([i**2])
+print(list2)
+print(list3)
